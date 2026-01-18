393f8dba6aedd883ef4f51ab4de779b2f0f932b4
sc02102670-arch <sc02102670@gmail.com>
2026-01-18 11:21:37 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   build: {
     outDir: 'dist',
-  }
+    rollupOptions: {
+      input: {
+        main: './index.html', // 시작점을 명시적으로 지정
+      },
+    },
+  },
 })
